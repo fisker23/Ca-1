d86514b8762cd39808fbae564d1864af022315d8
fisker23 <fisker23@hotmail.com>
2015-02-19 13:58:26 +0100

    HtttpServer addet + lidt htmlsjov

---
+++ src/Server/httpServer.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+/**
+ *
+ * @author Andreas Fisker
+ */
+public class httpServer {
+    
+    static int port = 8080;
+    static String ip = "127.0.0.1";
+    static String contentFolder = "public/";
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 2) {
+            port = Integer.parseInt(args[1]);
+            ip = args[0];
+        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
+        server.createContext("/", new RequestHandler());
+        server.setExecutor(null); // Use the default executor
+        server.start();
+        System.out.println("Server started, listening on port: " + port);
+    }
+
+    static class RequestHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            
+            File file = new File("public/index.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try{
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend,0,bytesToSend.length);
+            } catch(IOException ie){
+                ie.printStackTrace();
+                
+            }
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try(OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend,0,bytesToSend.length);
+            }
+        }
+    }
+
+}
